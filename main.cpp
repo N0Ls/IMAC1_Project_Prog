92d274f2851aa516e12688f40ea801016eaea87b
N0Ls <44846959+N0Ls@users.noreply.github.com>
2019-12-15 01:04:16 +0100

    Update Grid Drawing
    Drawing grid according to the game array

--- main.cpp
+++ main.cpp
@@ -7,12 +7,27 @@ using namespace std;
 
 
 
-void drawGrid(){
-  cout << "       | 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 |" << endl;
-  for(int i =0;i<10;i++){
-    cout << "       _________________________________________" << endl;
-    cout <<"  |" << i<<"|  " << "|   |   |   |   |   |   |   |   |   |   |" << endl;
-  }
+void drawGrid(int tabGrid[]){
+  cout << "      Y| 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 |" << endl;
+    cout << "    X  _________________________________________" << endl;
+    for (int i = 0; i <X_DIMENSION; i++) {
+      cout <<"|   " << i<<"  |";
+      for (int y = 0; y < Y_DIMENSION; y++) {
+
+        if(tabGrid[X_DIMENSION*i+y]== 1){
+          cout << " 1 |";
+        }
+        if(tabGrid[X_DIMENSION*i+y]== 2){
+          cout << " 2 |";
+        }
+        if(tabGrid[X_DIMENSION*i+y]== 0){
+          cout << "   |";
+        }
+      }
+      cout << endl;
+      cout << "       _________________________________________" << endl;
+    }
+
 }
 
 void initGrid(int *tabGrid){
@@ -22,20 +37,22 @@ void initGrid(int *tabGrid){
     }
   }
 }
-// void updateGrid(int *tabGrid,){
-//   for()
-// }
+void updateGrid(int *tabGrid,player player){
+  for(int i=0; i<player.nb_unite_active ; i++){
+    tabGrid[player.infantry_list[i].x*X_DIMENSION+player.infantry_list[i].y]=player.id;
+  }
+}
 
 
 int main(int argc, char const *argv[]) {
   int tableauGrid[X_DIMENSION * Y_DIMENSION];
   player playerTest;
-
   initPlayer(&playerTest,1,3);
   //changeCoordinates(&playerTest.infantry_list[0].x,&playerTest.infantry_list[0].y);
   placeUnits(&playerTest);
   printPlayer(playerTest);
   initGrid(tableauGrid);
-  drawGrid();
+  updateGrid(tableauGrid,playerTest);
+  drawGrid(tableauGrid);
   return 0;
 }
